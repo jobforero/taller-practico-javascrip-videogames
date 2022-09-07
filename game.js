5c8ff3eee879c743c91b324dfb593a7b55cc7ff5
jobforero <jobforero@gmail.com>
2022-09-07 08:14:06 -0500

    Subire el cambio del 09-07-2022

---
+++ game.js
@@ -0,0 +1,9 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener ('load', startGame);
+
+function startGame() {
+    //game.&*()
+    game.fillRect(0,0,0,0);
+}
